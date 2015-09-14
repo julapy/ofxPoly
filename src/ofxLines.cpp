f630040a85375a84dc46d0636dd2253ab3c65f19
lukasz karluk <julapy@googlemail.com>
2015-09-14 12:40:48 +1000

    first commit.

---
+++ src/ofxLines.cpp
@@ -0,0 +1,24 @@
+//
+//  ofxLines.cpp
+//  example
+//
+//  Created by Lukasz Karluk on 14/09/2015.
+//
+//
+
+#include "ofxLines.h"
+
+void ofxLinesGrow(const ofPolyline & polyIn, ofPolyline & polyOut, float amount) {
+    
+    polyOut.clear();
+    
+    const vector<ofVec3f> & points = polyIn.getVertices();
+    
+    for(int i=0; i<points.size(); i++) {
+        ofVec3f point = points[i];
+        ofVec3f normal = polyIn.getNormalAtIndex(i);
+        point += normal * amount;
+        
+        polyOut.addVertex(point);
+    }
+}
---
+++ src/ofxLines.h
@@ -0,0 +1,11 @@
+//
+//  ofxLines.h
+//  example
+//
+//  Created by Lukasz Karluk on 14/09/2015.
+//
+//
+
+#include "ofMain.h"
+
+void ofxLinesGrow(const ofPolyline & polyIn, ofPolyline & polyOut, float amount);
